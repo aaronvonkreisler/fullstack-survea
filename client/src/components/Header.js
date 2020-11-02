b5be82ceab0987afac7816eeddaa231cfd0b6a51
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-02 12:43:00 -0600

    created stripe checkout component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import StripeWrapper from './StripeWrapper';
 
 const Header = (props) => {
    console.log(props);
