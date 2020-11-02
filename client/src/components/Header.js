20e655c42e45cb157dab1388f6bf9787ac8e329d
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-02 17:47:52 -0600

    added stripe checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,10 +3,9 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import StripeWrapper from './StripeWrapper';
 
-const Header = (props) => {
-   console.log(props);
-   const renderContent = () => {
-      switch (props.auth) {
+class Header extends React.Component {
+   renderContent() {
+      switch (this.props.auth) {
          case null:
             return;
          case false:
@@ -17,27 +16,33 @@ const Header = (props) => {
             );
          default:
             return (
-               <li>
-                  <a href="/api/logout">Logout</a>
-               </li>
+               <React.Fragment>
+                  <li>
+                     <StripeWrapper />
+                  </li>
+                  <li>
+                     <a href="/api/logout">Logout</a>
+                  </li>
+               </React.Fragment>
             );
       }
-   };
-
-   return (
-      <nav className="blue accent-3">
-         <div className="nav-wrapper">
-            <Link
-               to={props.auth ? '/surveys' : '/'}
-               className="left brand-logo"
-            >
-               Survea
-            </Link>
-            <ul className="right">{renderContent()}</ul>
-         </div>
-      </nav>
-   );
-};
+   }
+   render() {
+      return (
+         <nav className="blue accent-3">
+            <div className="nav-wrapper">
+               <Link
+                  to={this.props.auth ? '/surveys' : '/'}
+                  className="left brand-logo"
+               >
+                  Survea
+               </Link>
+               <ul className="right">{this.renderContent()}</ul>
+            </div>
+         </nav>
+      );
+   }
+}
 
 const mapStateToProps = ({ auth }) => {
    return { auth };
